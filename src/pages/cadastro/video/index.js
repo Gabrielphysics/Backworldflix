3cc3929ecce4eaa673596a3a2e4e9ae7697d98ae
GabrielPhisics <gabrieljorge343@gmail.com>
2020-07-31 16:20:14 -0300

    só de zoeira

--- src/pages/cadastro/video/index.js
+++ src/pages/cadastro/video/index.js
@@ -8,6 +8,7 @@ function CadastroVideo(){
     return(
       <PageDefault>
        <h1 className="oi">Cadastro de video</h1>
+       <h1 className="oi">Fala Rael</h1>
        <Link to="/cadastro/categoria">
          Cadastrar Categoria
 
