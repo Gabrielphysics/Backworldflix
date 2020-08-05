7cd7b481e91e5245f88744445d29772226366adc
GabrielPhisics <gabrieljorge343@gmail.com>
2020-08-05 16:16:11 -0300

    depois de tanto tempo estudando a aula e dar um monte de pau .... mais um avanço

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import LOGO from '../../assets/img/LOGO.png';
-import './Menu.css'
+import './Menu.css';
 import Button from '../Button';
-//import ButtonLink from './components/ButtonLink';
+// import ButtonLink from './components/ButtonLink';
 
-function Menu () {
-    return (
-        <nav className="Menu">
-            <Link to="/">
-                <img className="Logo" src={LOGO} alt="back world logo"/>
-            </Link>
+function Menu() {
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={LOGO} alt="back world logo" />
+      </Link>
 
-            <Button as={Link} className="ButtonLink" to="/cadastro/video">
-                Novo Vídeo!
-            </Button>
-        </nav>
-    );
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo Vídeo!
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,35 +1,130 @@
-import React from 'react';
+/* eslint-disable linebreak-style */
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
+function CadastroCategoria() {
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
 
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  }
 
-function CadastroCategoria(){
-    return(
-      <PageDefault>
-       <h1 className="oi">Cadastro de Categoria</h1>
-       
-        <form>
-
-          <label>
-            Nome da Categoria:
-            <input
-              type="text"
-            />
-          </label>
-
-          <button className="oi">
-            Cadastrar
-          </button>
-        </form>
-
-
-       <Link to="/">
-         Ir para Home
-
-       </Link>
-      </PageDefault>
+  function handleChange(infosDoEvento) {
+    setValue(
+      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.value,
     );
+  }
+
+  useEffect(() => {
+    console.log('hi');
+    const URL_TOP = 'http://localhost:8080/categorias';
+    fetch(URL_TOP).then(async (respostaDoServidor) => {
+      const resposta = await respostaDoServidor.json();
+      setCategorias([
+        ...resposta,
+      ]);
+    });
+
+    /*     setTimeout(() => {
+      setCategorias([
+        {
+          id: 1,
+          nome: 'Front End',
+          descricao: 'Brabo',
+          cor: '#cbd1ff',
+        },
+        {
+          id: 2,
+          nome: 'back End',
+          descricao: 'Braboass',
+          cor: '#cbd1ff',
+        },
+      ]);
+    }, 4 * 1000); */
+  }, []);
+
+  return (
+    <PageDefault>
+      <h1 className="oi">
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
+
+      <form onSubmit={function handleSubmit(infosDoEvento) {
+        infosDoEvento.preventDefault();
+        setCategorias([
+          ...categorias,
+          values,
+        ]);
+
+        setValues(valoresIniciais);
+      }}
+      >
+
+        <FormField
+          label="Nome da Categoria: "
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição: "
+          type="textarea"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Cor: "
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+
+        <Button className="oi">
+          Cadastrar
+        </Button>
+
+      </form>
+
+      {categorias.length === 0 && (
+        <div>
+          Loading...
+        </div>
+      )}
+
+      <ul>
+        {categorias.map((categoria, indice) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <li key={`${categoria}${indice}`}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/">
+        Ir para Home
+
+      </Link>
+    </PageDefault>
+  );
 }
 
 export default CadastroCategoria;
